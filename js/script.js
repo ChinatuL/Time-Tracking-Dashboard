c3337e6f3802b6697d5d1b8b5631d67de13a297f
Chinatu Lucia Eke <chinatuluciaeke@gmail.com>
2023-04-14 04:44:00 +0100

    add timeframe functionality

---
+++ js/script.js
@@ -0,0 +1,96 @@
+const dailyBtn = document.getElementById("daily");
+const weeklyBtn = document.getElementById("weekly");
+const monthlyBtn = document.getElementById("monthly");
+const currentStatus = document.querySelectorAll(".tracker__card-current");
+const previousStatus = document.querySelectorAll(".tracker__card-previous");
+
+dailyBtn.addEventListener("click", () => {
+    dailyBtn.classList.add("active");
+    weeklyBtn.classList.remove("active");
+    monthlyBtn.classList.remove("active");
+    getDailyData();
+});
+
+weeklyBtn.addEventListener("click", () => {
+    dailyBtn.classList.remove("active");
+    weeklyBtn.classList.add("active");
+    monthlyBtn.classList.remove("active");
+    getWeeklyData();
+});
+
+monthlyBtn.addEventListener("click", () => {
+    dailyBtn.classList.remove("active");
+    weeklyBtn.classList.remove("active");
+    monthlyBtn.classList.add("active");
+    getMonthlyData();
+});
+
+const getDailyData = () => {
+    fetch("../data.json")
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            data.forEach((stat) => {
+                currentStatus.forEach((element, index) => {
+                    currentStatus[index].innerHTML =
+                        data[index].timeframes.daily.current + "hrs";
+                    previousStatus[
+                        index
+                    ].innerHTML = `Last Week - ${data[index].timeframes.daily.previous}hrs`;
+                });
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+};
+
+const getWeeklyData = () => {
+    fetch("../data.json")
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            data.forEach((stat) => {
+                currentStatus.forEach((element, index) => {
+                    currentStatus[index].innerHTML =
+                        data[index].timeframes.weekly.current + "hrs";
+                    previousStatus[
+                        index
+                    ].innerHTML = `Last Week - ${data[index].timeframes.weekly.previous}hrs`;
+                });
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+};
+
+const getMonthlyData = () => {
+    fetch("../data.json")
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            data.forEach((stat) => {
+                currentStatus.forEach((element, index) => {
+                    currentStatus[index].innerHTML =
+                        data[index].timeframes.monthly.current + "hrs";
+                    previousStatus[
+                        index
+                    ].innerHTML = `Last Week - ${data[index].timeframes.monthly.previous}hrs`;
+                });
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+};
+
+window.addEventListener("load", () => {
+    daily.classList.remove("active");
+    weeklyBtn.classList.add("active");
+    monthlyBtn.classList.remove("active");
+    getWeeklyData();
+});
